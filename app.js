19cd567e4e3724882df27e7401963f1b0380d5d4
Wei Yang <lohwei321@gmail.com>
2023-07-30 00:33:35 +0800

    Add modification to get and put methods to allow save push requests to customers.json

--- app.js
+++ app.js
@@ -2,38 +2,93 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
+const fs = require("fs");
 
-const customers = [
-  {
-    ID: 1,
-    Name: "Wei Yang",
-    Gender: "Male",
-  },
-];
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Testing...");
 });
 
 app.get("/api/customers", (req, res) => {
-  res.send(customers);
+  fs.readFile("customers.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Error reading the JSON file." });
+    }
+
+    const customerData = JSON.parse(data);
+    res.json(customerData);
+  });
 });
 
 app.get("/api/customers/:ID", (req, res) => {
-  const customer = customers.find((i) => i.ID === parseInt(req.params.ID));
-  if (!customer) res.status(404).send("The customer does not exist");
-  res.send(customer);
+  const customerID = parseInt(req.params.ID);
+
+  if (isNaN(customerID)) {
+    res.status(400).json({ error: "Invalid data ID provided." });
+  }
+
+  fs.readFile("customers.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Error reading the JSON file." });
+    }
+
+    const customerData = JSON.parse(data);
+    const Customers = customerData.Customers;
+
+    const foundCustomer = Customers.find(
+      (customer) => customer.ID === customerID
+    );
+
+    if (!foundCustomer) {
+      res.status(404).json({ error: "Customer does not exist." });
+    }
+
+    res.json(foundCustomer);
+  });
 });
 
 app.post("/api/customers", (req, res) => {
-  const customer = {
-    ID: customers.length + 1,
-    Name: req.body.Name,
-    Gender: req.body.Gender,
-  };
-  customers.push(customer);
-  res.send(customer);
+  try {
+    const newCustomer = req.body;
+
+    if (!newCustomer || !newCustomer.Name || !newCustomer.Gender) {
+      res
+        .status(400)
+        .json({ error: "Invalid data format. Name and gender is required." });
+    }
+
+    fs.readFile("customers.json", "utf8", (err, data) => {
+      if (err) {
+        res.status(500).json({ error: "Error reading the JSON file." });
+      }
+
+      const customerData = JSON.parse(data);
+      const Customers = customerData.Customers;
+
+      const newCustomerData = {
+        ID: Customers.length + 1,
+        Name: newCustomer.Name,
+        Gender: newCustomer.Gender,
+      };
+
+      Customers.push(newCustomerData);
+
+      fs.writeFile(
+        "customers.json",
+        JSON.stringify(customerData, null, 2),
+        (err) => {
+          if (err) {
+            res.status(500).json({ error: "Error writing to the JSON file." });
+          }
+
+          res.json(newCustomerData);
+        }
+      );
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error." });
+  }
 });
 
 app.put("/api/customers/:ID", (req, res) => {
