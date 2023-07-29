2eb6f1f29f74d796183f35771caf53d47ada772c
Wei Yang <lohwei321@gmail.com>
2023-07-29 17:55:39 +0800

    Add get method for specific ID and post method

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ const express = require("express");
 
 const app = express();
 
-const customers = [{
-    ID : 1,
-    Name : "Wei Yang"
-}]
+app.use(express.json());
 
-app.get("/",(req,res) => {
-    res.send("Testing...")
-})
+const customers = [
+  {
+    ID: 1,
+    Name: "Wei Yang"
+  },
+];
 
-app.get("/api/customers",(req,res) => {
-    res.send(customers)
-})
+app.get("/", (req, res) => {
+  res.send("Testing...");
+});
+
+app.get("/api/customers", (req, res) => {
+  res.send(customers);
+});
+
+app.get("/api/customers/:ID", (req, res) => {
+    const customer = customers.find(i => i.ID === parseInt(req.params.ID))
+    if (!customer) res.status(404).send("The customer does not exist")
+    res.send(customer);
+  });
+
+app.post("/api/customers", (req, res) => {
+  const customer = {
+    ID: customers.length + 1,
+    Name: req.body.Name,
+  };
+  customers.push(customer);
+  res.send(customer);
+});
 
 app.listen(3000, () => console.log(`Server running on port:3000`));
